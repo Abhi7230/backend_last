16ab842bdd0a7e7100c2e0b1295033afebf8181e
Abhi7230 <105153898+Abhi7230@users.noreply.github.com>
2024-07-27 14:57:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,10 +28,12 @@ const corsOptions = {
 };
 // On the server
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://frontend-last-tau.vercel.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', 'https://frontend-last-tau.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
+
 
 
 app.use(cors(corsOptions));
